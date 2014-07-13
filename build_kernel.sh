65482efdf8fb22ae0a8dc1f15f1a99a04735ac7b
davepmer <davepmer20102010@gmail.com>
2014-07-12 20:40:43 -0400

    USB-OTG

--- build_kernel.sh
+++ build_kernel.sh
@@ -18,9 +18,10 @@ export CROSS_COMPILE=/home/dave/toolchain/arm-eabi-4.7/bin/arm-eabi-
 # make zImage
 ##############################################################################
 mkdir -p ./obj/KERNEL_OBJ/
-make O=./obj/KERNEL_OBJ/ dave_cpu_overclock_m470_defconfig
+# make O=./obj/KERNEL_OBJ/ dave_cpu_overclock_m470_defconfig
 # make O=./obj/KERNEL_OBJ/ dave_m470_defconfig
-# make O=./obj/KERNEL_OBJ/ dave_no_overclock_m470_defconfig
+make O=./obj/KERNEL_OBJ/ dave_no_overclock_m470_defconfig
+# make O=./obj/KERNEL_OBJ/ dave_no_oc_cq_m470_defconfig
 make -j6 O=./obj/KERNEL_OBJ/ # 2>&1 | tee log.txt
 
 ##############################################################################
